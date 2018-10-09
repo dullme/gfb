6fd4d9d673414e73e1d6984a9067dfe860d808a1
JINJIALEI <admin@dullme.com>
2018-10-09 14:52:48 +0800

    初始提交

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CapitalPool;
+use App\Models\Complex;
+use App\Models\Withdraw;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Predis\Client;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     function getTodayAmount(){
-        $redis = new \Predis\Client();
+        $redis = new Client(config('database.redis.default'));
         $a_amount = $redis->keys('a_*_' . date('Ymd'));
         $today_amount = $a_amount ? collect($redis->mget($a_amount))->sum() : 0;
         $v_amount = $redis->keys('v_*_' . date('Ymd'));
@@ -20,9 +24,9 @@ class Controller extends BaseController
 
 
         return [
-            'ad_fee' => \App\Models\CapitalPool::all()->sum('price'), //广告费总额
-            'amount' => \App\Models\Complex::all()->sum('history_amount') + $today_amount,    //分润总额
-            'withdraw' => \App\Models\Withdraw::where('status', 1)->get()->sum('price'),  //提现总额
+            'ad_fee' => CapitalPool::all()->sum('price'), //广告费总额
+            'amount' => Complex::all()->sum('history_amount') + $today_amount,    //分润总额
+            'withdraw' => Withdraw::where('status', 1)->get()->sum('price'),  //提现总额
             'visits' => $visits  //今日访问人数
         ];
     }
