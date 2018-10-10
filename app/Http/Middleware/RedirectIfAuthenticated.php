605eb3028bbd2ce5439a7edaf2b13258bf416adb
JINJIALEI <admin@dullme.com>
2018-10-10 09:31:42 +0800

    初始提交

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return response()->json(['message' => '认证失败'], 422);
         }
 
         return $next($request);
