28b74595fec8dccfb41fa9aa4bdd446e9c672056
dullme <admin@dullme.com>
2019-08-05 21:45:44 +0800

    修改api接口状态码

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,13 +1,13 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover">
-        <title>{{ $page->title }}</title>
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover">
+    <title>{{ $page->title }}</title>
+</head>
+<body>
     <body style="margin: 0; padding: 0;">
-        <div style="width: 100%">
-            {!! $page->text !!}
-        </div>
+        {!! $page->text !!}
     </body>
+</body>
 </html>
